4e4fd8a92b05ebbcb7b6038fae37e138236df8fd
rlyost <ry@ryost.us>
2018-03-11 16:37:17 -0700

    added validation for Name and comments

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
+app.secret_key = 'KeepItSecretKeepItSafe'
 
 # our index route will handle rendering our form
 @app.route('/')
@@ -10,12 +11,20 @@ def index():
 # notice how we defined which HTTP methods are allowed by this route
 @app.route('/survey', methods=['POST'])
 def survey_info():
-  print "Got it"
-
   name = request.form['name']
   location = request.form['location']
   language = request.form['language']
   comment = request.form['comment']
+  if len(name) < 1:
+    # display validation errors
+    flash("Name can not be empty!")
+    return redirect('/')
+  if len(comment) < 1:
+    flash("Comment can not be empty!")
+    return redirect('/')
+  elif len(comment) > 120:
+    flash("Comment is too long. No more then 120 characters.")
+    return redirect('/')
 
   return render_template("process.html", name2=name, location2=location, language2=language, comment2=comment)
 
